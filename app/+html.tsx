ad210159004e1fa38a35fd61d985af63c5c0079f
Dtsg02 <danielturzaprivate@gmail.com>
2026-02-26 16:45:36 +0100

    Use zoomed web tab favicon

--- app/+html.tsx
+++ app/+html.tsx
@@ -14,6 +14,7 @@ export default function Root({ children }: PropsWithChildren) {
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
         <meta name="apple-mobile-web-app-title" content="TLF TrackerX" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon.png" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
       </head>
       <body>
